a1b0b7f1f9c18021b5e74a3e377d2549e1484441
guillaumequillet <qgdevfr@gmail.com>
2026-01-18 10:25:59 +0100

    Scene management

--- init.rb
+++ init.rb
@@ -1,9 +1,13 @@
 require 'gosu'
 
+Dir.glob("lib/*.rb").each {|fn| require_relative(fn)}
+
 class Window < Gosu::Window
     def initialize
         super(640, 480, false)
         self.caption = 'Gosu Game Jam 9'
+        @scene = SceneGame.new(self)
+        @font = Gosu::Font.new(24)
     end
 
     def needs_cursor?; false; end
@@ -14,11 +18,25 @@ class Window < Gosu::Window
     end
 
     def update
+        case @scene.class.to_s
+        when 'SceneTitle'
 
+        when 'SceneGame'
+
+        when 'SceneGameOver'
+
+        end
     end
 
     def draw
-
+        case @scene.class.to_s
+        when 'SceneTitle'
+            @font.draw_text('TITLE SCREEN', 50, 50, 0)
+        when 'SceneGame'
+            @font.draw_text('GAME SCREEN', 50, 50, 0)
+        when 'SceneGameOver'
+            @font.draw_text('GAME OVER', 50, 50, 0)
+        end
     end
 end
 
---
+++ lib/Scene.rb
@@ -0,0 +1,29 @@
+class Scene
+    def initialize(window)
+        @window = window
+    end
+
+    def button_down(id)
+
+    end
+
+    def update
+
+    end
+
+    def draw
+
+    end
+end
+
+class SceneTitle < Scene
+
+end
+
+class SceneGame < Scene
+
+end
+
+class SceneGameOver < Scene
+
+end
